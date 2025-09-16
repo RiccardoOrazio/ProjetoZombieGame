9b8ebf5a33cd9f2758ce6fa227aec99ce1a173c3
RiccardoOrazio <riccardo.orazio@iesb.edu.br>
2025-09-16 18:17:22 -0300

    Muzzle do Tiro

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
+using UnityEngine.VFX; // Necessário para controlar o Visual Effect
 
 public class PlayerShooting : MonoBehaviour
 {
     [Header("Referências")]
     [SerializeField] private GameObject projectilePrefab;
-    [SerializeField] private Transform firePointRight; 
-    [SerializeField] private Transform firePointLeft; 
-    [SerializeField] private IsometricPlayerMovement playerMovement; 
+    [SerializeField] private Transform firePointRight;
+    [SerializeField] private Transform firePointLeft;
+    [SerializeField] private IsometricPlayerMovement playerMovement;
+    [SerializeField] private VisualEffect muzzleFlashRight;
+    [SerializeField] private VisualEffect muzzleFlashLeft;
 
     [Header("Configurações do Tiro")]
     [SerializeField] private float projectileSpeed = 20f;
+
+    void Start()
+    {
+        if (muzzleFlashRight != null) muzzleFlashRight.Stop();
+        if (muzzleFlashLeft != null) muzzleFlashLeft.Stop();
+    }
 
     void Update()
     {
@@ -22,6 +31,12 @@ public class PlayerShooting : MonoBehaviour
     void Shoot()
     {
         Transform currentFirePoint = playerMovement.IsCurrentlyFacingRight ? firePointRight : firePointLeft;
+        VisualEffect currentMuzzleFlash = playerMovement.IsCurrentlyFacingRight ? muzzleFlashRight : muzzleFlashLeft;
+
+        if (currentMuzzleFlash != null)
+        {
+            currentMuzzleFlash.Play();
+        }
 
         GameObject projectile = Instantiate(projectilePrefab, currentFirePoint.position, currentFirePoint.rotation);
 
